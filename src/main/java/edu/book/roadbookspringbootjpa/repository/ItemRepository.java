5b4073cc30ff0ff6daade222671eefe73fee70e4
tasklet1579 <tasklet1579@gmail.com>
2022-05-07 08:58:41 +0900

    상품명 조회 테스트

--- src/main/java/edu/book/roadbookspringbootjpa/repository/ItemRepository.java
+++ src/main/java/edu/book/roadbookspringbootjpa/repository/ItemRepository.java
@@ -3,6 +3,9 @@ package edu.book.roadbookspringbootjpa.repository;
 import edu.book.roadbookspringbootjpa.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+    List<Item> findByItemNm(String itemNm);
 }
--- src/test/java/edu/book/roadbookspringbootjpa/repository/ItemRepositoryTest.java
+++ src/test/java/edu/book/roadbookspringbootjpa/repository/ItemRepositoryTest.java
@@ -11,6 +11,7 @@ import org.springframework.test.context.TestPropertySource;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.properties")
@@ -35,5 +36,29 @@ class ItemRepositoryTest {
         item.setUpdateTime(LocalDateTime.now());
         Item savedItem = itemRepository.save(item);
         System.out.println(savedItem);
+    }
+
+    public void createItemList() {
+        for (int i = 1; i <= 10; i++) {
+            Item item = new Item();
+            item.setItemNm("테스트 상품" + i);
+            item.setPrice(10000 + i);
+            item.setItemDetail("테스트 상품 상세 설명" + i);
+            item.setItemSellStatus(ItemSellStatus.SELL);
+            item.setStockNumber(100);
+            item.setRegTime(LocalDateTime.now());
+            item.setUpdateTime(LocalDateTime.now());
+            Item savedItem = itemRepository.save(item);
+        }
+    }
+
+    @Test
+    @DisplayName("상품명 조회 테스트")
+    public void findByItemNmTest() {
+        this.createItemList();
+        List<Item> itemList = itemRepository.findByItemNm("테스트 상품1");
+        for (Item item : itemList) {
+            System.out.println(item.toString());
+        }
     }
 }
