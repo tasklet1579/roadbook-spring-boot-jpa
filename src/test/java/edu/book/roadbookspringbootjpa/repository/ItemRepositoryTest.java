359f1b8e9a3e9475aca40d4cb643a0f9f23d3602
tasklet1579 <tasklet1579@gmail.com>
2022-05-07 08:57:35 +0900

    상품 저장 테스트

---
+++ src/test/java/edu/book/roadbookspringbootjpa/repository/ItemRepositoryTest.java
@@ -0,0 +1,39 @@
+package edu.book.roadbookspringbootjpa.repository;
+
+import edu.book.roadbookspringbootjpa.constant.ItemSellStatus;
+import edu.book.roadbookspringbootjpa.entity.Item;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+class ItemRepositoryTest {
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    @DisplayName("상품 저장 테스트")
+    public void createItemTest() {
+        Item item = new Item();
+        item.setItemNm("테스트 상품");
+        item.setPrice(10000);
+        item.setItemDetail("테스트 상품 상세 설명");
+        item.setItemSellStatus(ItemSellStatus.SELL);
+        item.setStockNumber(100);
+        item.setRegTime(LocalDateTime.now());
+        item.setUpdateTime(LocalDateTime.now());
+        Item savedItem = itemRepository.save(item);
+        System.out.println(savedItem);
+    }
+}
