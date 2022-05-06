e03be0b9a4d5a415f0c9b15e82d59513011d29b8
tasklet1579 <tasklet1579@gmail.com>
2022-05-07 08:39:23 +0900

    프로젝트 생성

---
+++ src/main/java/edu/book/roadbookspringbootjpa/RoadbookSpringBootJpaApplication.java
@@ -0,0 +1,12 @@
+package edu.book.roadbookspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadbookSpringBootJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoadbookSpringBootJpaApplication.class, args);
+    }
+}
---
+++ src/test/java/edu/book/roadbookspringbootjpa/RoadbookSpringBootJpaApplicationTests.java
@@ -0,0 +1,12 @@
+package edu.book.roadbookspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadbookSpringBootJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
