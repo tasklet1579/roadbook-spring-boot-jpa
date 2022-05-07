42fb40dfa6d71df9ec5d6135536d19de152319f9
tasklet1579 <tasklet1579@gmail.com>
2022-05-07 09:01:16 +0900

    @Query를 이용한 상품 조회 테스트

--- src/main/java/edu/book/roadbookspringbootjpa/repository/ItemRepository.java
+++ src/main/java/edu/book/roadbookspringbootjpa/repository/ItemRepository.java
@@ -2,6 +2,8 @@ package edu.book.roadbookspringbootjpa.repository;
 
 import edu.book.roadbookspringbootjpa.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,4 +16,7 @@ public interface ItemRepository extends JpaRepository<Item, Long> {
     List<Item> findByPriceLessThan(Integer price);
 
     List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like %:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
 }
