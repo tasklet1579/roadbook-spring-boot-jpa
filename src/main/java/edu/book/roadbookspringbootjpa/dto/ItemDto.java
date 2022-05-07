5414eb39eaf1222319f6922da00c341970f38c0f
tasklet1579 <tasklet1579@gmail.com>
2022-05-07 09:17:19 +0900

    상품 데이터 출력 예제

---
+++ src/main/java/edu/book/roadbookspringbootjpa/dto/ItemDto.java
@@ -0,0 +1,25 @@
+package edu.book.roadbookspringbootjpa.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ItemDto {
+
+    private Long id;
+
+    private String itemNm;
+
+    private int price;
+
+    private String itemDetail;
+
+    private String sellStatCd;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
