f14c623aea97f40003447def3bb659428467573e
tasklet1579 <tasklet1579@gmail.com>
2022-05-08 20:55:05 +0900

    회원가입 및 데이터 유효성 검증

--- src/main/java/edu/book/roadbookspringbootjpa/controller/HomeController.java
+++ src/main/java/edu/book/roadbookspringbootjpa/controller/HomeController.java
@@ -1,13 +1,5 @@
 package edu.book.roadbookspringbootjpa.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
 public class HomeController {
 
-    @GetMapping(value = "/")
-    public String HelloWorld() {
-        return "Hello World";
-    }
 }
