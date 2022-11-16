e21d382a32122a7101f87e6d42ed137b466620a7
tasklet1579 <tasklet1579@gmail.com>
2022-11-16 15:49:15 +0900

    다대일/일대다 양방향 매핑하기

---
+++ src/main/java/edu/book/roadbookspringbootjpa/constant/OrderStatus.java
@@ -0,0 +1,5 @@
+package edu.book.roadbookspringbootjpa.constant;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
---
+++ src/main/java/edu/book/roadbookspringbootjpa/entity/Order.java
@@ -0,0 +1,37 @@
+package edu.book.roadbookspringbootjpa.entity;
+
+import edu.book.roadbookspringbootjpa.constant.OrderStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "orders") // 정렬할 때 사용하는 order 키워드가 있기 때문에
+public class Order {
+    @Id
+    @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @ManyToOne // 회원은 여러 번 주문할 수 있다
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus orderStatus;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
