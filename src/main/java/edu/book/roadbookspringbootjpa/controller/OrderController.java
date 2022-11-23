8506009d4bf885cb775832960208071c647e3bfb
tasklet1579 <tasklet1579@gmail.com>
2022-11-23 23:55:36 +0900

    주문 이력 조회하기

--- src/main/java/edu/book/roadbookspringbootjpa/controller/OrderController.java
+++ src/main/java/edu/book/roadbookspringbootjpa/controller/OrderController.java
@@ -1,20 +1,24 @@
 package edu.book.roadbookspringbootjpa.controller;
 
 import edu.book.roadbookspringbootjpa.dto.OrderDto;
+import edu.book.roadbookspringbootjpa.dto.OrderHistDto;
 import edu.book.roadbookspringbootjpa.service.OrderService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,9 +26,10 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping(value = "/order")
-    public @ResponseBody ResponseEntity<?> order(@RequestBody @Valid OrderDto orderDto,
-                                                 BindingResult bindingResult,
-                                                 Principal principal) {
+    public @ResponseBody
+    ResponseEntity<?> order(@RequestBody @Valid OrderDto orderDto,
+                            BindingResult bindingResult,
+                            Principal principal) {
         if (bindingResult.hasErrors()) {
             StringBuilder sb = new StringBuilder();
             List<FieldError> fieldErrors = bindingResult.getFieldErrors();
@@ -45,5 +50,17 @@ public class OrderController {
         }
 
         return new ResponseEntity<>(orderId, HttpStatus.OK);
+    }
+
+    @GetMapping(value = {"orders", "/orders/{page}"})
+    public String orderHist(@PathVariable("page") Optional<Integer> page, Principal principal, Model model) {
+        Pageable pageable = PageRequest.of(page.orElse(0), 4);
+
+        Page<OrderHistDto> orderHistDtoList = orderService.getOrderList(principal.getName(), pageable);
+
+        model.addAttribute("orders", orderHistDtoList);
+        model.addAttribute("page", pageable.getPageNumber());
+        model.addAttribute("maxPage", 5);
+        return "order/orderHist";
     }
 }
--- src/main/java/edu/book/roadbookspringbootjpa/service/OrderService.java
+++ src/main/java/edu/book/roadbookspringbootjpa/service/OrderService.java
@@ -1,14 +1,17 @@
 package edu.book.roadbookspringbootjpa.service;
 
 import edu.book.roadbookspringbootjpa.dto.OrderDto;
-import edu.book.roadbookspringbootjpa.entity.Item;
-import edu.book.roadbookspringbootjpa.entity.Member;
-import edu.book.roadbookspringbootjpa.entity.Order;
-import edu.book.roadbookspringbootjpa.entity.OrderItem;
+import edu.book.roadbookspringbootjpa.dto.OrderHistDto;
+import edu.book.roadbookspringbootjpa.dto.OrderItemDto;
+import edu.book.roadbookspringbootjpa.entity.*;
+import edu.book.roadbookspringbootjpa.repository.ItemImgRepository;
 import edu.book.roadbookspringbootjpa.repository.ItemRepository;
 import edu.book.roadbookspringbootjpa.repository.MemberRepository;
 import edu.book.roadbookspringbootjpa.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,6 +26,7 @@ public class OrderService {
     private final ItemRepository itemRepository;
     private final MemberRepository memberRepository;
     private final OrderRepository orderRepository;
+    private final ItemImgRepository itemImgRepository;
 
     public Long order(OrderDto orderDto, String email) {
         Item item = itemRepository.findById(orderDto.getItemId())
@@ -37,5 +41,29 @@ public class OrderService {
         orderRepository.save(order);
 
         return order.getId();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<OrderHistDto> getOrderList(String email, Pageable pageable) {
+        List<Order> orders = orderRepository.findOrders(email, pageable);
+        Long totalCount = orderRepository.countOrder(email);
+
+        List<OrderHistDto> orderHistDtos = new ArrayList<>();
+
+        for (Order order : orders) {
+            OrderHistDto orderHistDto = new OrderHistDto(order);
+            List<OrderItem> orderItems = order.getOrderItems(); // 사이즈만큼 조회 쿼리 발생
+
+            for (OrderItem orderItem : orderItems) {
+                ItemImg itemImg = itemImgRepository.findByItemIdAndRepimgYn(orderItem.getItem()
+                                                                                     .getId(), "Y");
+                OrderItemDto orderItemDto = new OrderItemDto(orderItem, itemImg.getImgUrl());
+                orderHistDto.addOrderItemDto(orderItemDto);
+            }
+
+            orderHistDtos.add(orderHistDto);
+        }
+
+        return new PageImpl<>(orderHistDtos, pageable, totalCount);
     }
 }
